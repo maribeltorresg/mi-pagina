e8f9de0f1c48f29d9c2a7649adb84f04c3de498a
maribeltorresg <maribel.torres.guevara@gmail.com>
2017-12-09 19:58:41 -0500

    subiendo la version desktop

---
+++ js/app.js
@@ -0,0 +1,6 @@
+window.addEventListener('scroll', function(e) {
+    var mainNav = document.getElementById('main-nav');
+    // console.log(mainNav);
+    mainNav.classList.add('background');
+    console.log(e);
+})
