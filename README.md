9140497d9934062c896a4ef299d876f16acbd255
maribeltorresg <maribel.torres.guevara@gmail.com>
2017-12-11 01:49:25 -0500

    agregando interacción al menu-hamburguesa

---
+++ README.md
@@ -0,0 +1,5 @@
+# Reto 2: Mi website
+*  **Curso**: Responsive web Design
+*  **Unidad**: La web desde un móvil
+## OBJETIVO
+Realizar una página con un diseño a nuestro gusto, pero debemos mantener la estructura y usando un grid (cualquiera pero que no sea un framework como Bootstrap o Materialize).
--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
-window.addEventListener('scroll', function(e) {
-    var mainNav = document.getElementById('main-nav');
-    // console.log(mainNav);
-    mainNav.classList.add('background');
-    console.log(e);
-})
+window.addEventListener('scroll', function() {
+  var mainNav = document.getElementById('main-nav');
+  // console.log(mainNav);
+  mainNav.classList.add('background');
+  // console.log(e);
+});
+
+window.addEventListener('load', function() {
+  var boton = document.getElementById('boton');
+  console.log(boton);
+  boton.addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    console.log(menu)
+    // verificamos si tiene el contenido 
+    if (menu.classList.contains('disabled-menu')) {
+      // console.log('mostrar');
+      // remover
+      menu.classList.remove('disabled-menu');
+      // agregar
+      menu.classList.add('enabled-menu');
+    } else {
+      // console.log('ocultar');
+      menu.classList.remove('enebled-menu');
+      menu.classList.add('disabled-menu');
+    }
+  });
+});
